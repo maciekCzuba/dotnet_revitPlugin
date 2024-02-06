538b010613d57d025cf4a2e03235c776e2f3320f
mtczu_Aecom <m.t.czuba@gmail.com>
2024-02-06 01:16:26 +0100

    Add project files.

---
+++ dotnet_revitPlugin/GetElementId.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace dotnet_revitPlugin
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class GetElementId : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            try
+            {
+                Reference pickObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if (pickObj != null)
+                {
+                    TaskDialog.Show("Element Id", pickObj.ElementId.ToString());
+                }
+
+                return Result.Succeeded;
+            }
+
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
