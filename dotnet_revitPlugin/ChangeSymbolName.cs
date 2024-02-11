93565883de60715630fb8b2ed5add45e87043e5c
mtczu_Aecom <m.t.czuba@gmail.com>
2024-02-11 17:10:04 +0100

    Add new commands

---
+++ dotnet_revitPlugin/ChangeSymbolName.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace dotnet_revitPlugin
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    internal class ChangeSymbolName : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+                Reference pickedObj = uidoc.Selection.PickObject(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if(pickedObj != null)
+                {
+                    ElementId eleId = pickedObj.ElementId;
+                    Element ele = doc.GetElement(eleId);
+
+                    Parameter eleParameter = ele.get_Parameter(BuiltInParameter.SYMBOL_ID_PARAM);
+                    ElementId symbolId = eleParameter.AsElementId();
+                    FamilySymbol symbol = doc.GetElement(symbolId) as FamilySymbol;
+                    
+                    using(var t = new Transaction(doc, "Rename Family Type"))
+                    {
+                        t.Start();
+                        string tmp = symbol.Name;
+                        symbol.Name = tmp + "_WIP";
+                        t.Commit();
+
+                        TaskDialog.Show("Output msg", $"'{tmp}' name was changed to" + Environment.NewLine +
+                        $"'{symbol.Name}'");
+                    }
+                }
+
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.ToString();
+                return Result.Failed;
+            }        
+        }
+    }
+}
---
+++ dotnet_revitPlugin/GetElementsName.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace dotnet_revitPlugin
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    internal class GetElementsName : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            try
+            {
+                IList<Reference> pickedObjs = uidoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element);
+
+                if(pickedObjs.Count > 0 )
+                {
+                    var eleDictionary = new Dictionary<ElementId, string>();
+
+                    foreach(Reference obj in pickedObjs)
+                    {
+                        ElementId eleId = obj.ElementId;
+                        Element ele = doc.GetElement(eleId);
+
+                        eleDictionary.Add(eleId, ele.Name);                        
+                    }
+
+                    var tDialog = new TaskDialog("Output msg");
+                    tDialog.MainInstruction = "'Element Name' : 'Element Id'";
+                    
+                    foreach (var kvp in eleDictionary)
+                    {
+                        tDialog.MainContent += $"{kvp.Value} : {kvp.Key}" + Environment.NewLine;
+                    }
+                    tDialog.Show();
+                }
+
+                else
+                {
+                    TaskDialog.Show("Output msg", "No element selected");
+                }
+
+                
+                return Result.Succeeded;
+            }
+
+            catch(Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
